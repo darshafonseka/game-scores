7f0753e8a51145e6eaf2f1d2cb8e5698f7c0106e
darshafonseka <darsha.fonseka@acentura.com>
2024-11-26 21:47:43 +0530

    query and server side optimization

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -38,3 +38,9 @@ CREATE TABLE `game_scores`.`user_game` (
 
 ALTER TABLE `game_scores`.`user_game`
 CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ;
+
+## Added an index on email assuming you search users by email address. this is done to optimize queries
+CREATE INDEX idx_email ON user (email);
+
+## stored procs and views could have been used for more optimizations but debugging would have been difficults
+## from server side caching could have been introduced if time permitted
